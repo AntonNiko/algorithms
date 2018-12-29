b253e052942f02125cf30dd4b8f0ce6b5191ca53
AntonNiko <ant.nikitenko@gmail.com>
2018-12-28 20:40:06 -0700

    Added comments and docstring for Selection Sort test script

--- big-o-analysis/list_sort.py
+++ big-o-analysis/list_sort.py
@@ -1,19 +1,27 @@
 #!/home/anton/anaconda3/bin/python
-import random
+
+from datetime import datetime
 import matplotlib.pyplot as plt
 from matplotlib.markers import MarkerStyle
 import numpy as np
-from datetime import datetime
+import random
 
+## Maximum size of data to test algorithm
 N = 200
+## Number of iterations when testing each size
 ITERATIONS = 10
 
-
 def plot_test_time_scatter(sizes, times):
-    rx, ry = 0.5, 0.5
-    area = 0.5 * 0.5 * np.pi
-    theta = np.arange(0, 2 * np.pi + 0.01, 0.1)
-    
+    """Plots a scatter plot of data size and times using matplotlib
+
+    Args:
+        sizes (list): List of size x of numbers that represent a size
+                      related to data used for testing
+        times (list): List of size x of float or integer numbers that
+                      represent a time related to data of size x
+    """
+
+    ## Setup size, color, and corresponding x & y coordinates
     area = np.pi * 3
     colors = (0,0,0) 
     x = np.array(sizes)
@@ -21,9 +29,17 @@ def plot_test_time_scatter(sizes, times):
     fig, ax = plt.subplots()
     ax.scatter(x, y, s = area, c = colors)
 
+    ## Plot scatter plot of test time
     plt.show()
 
 def smallest_elem_index(input_list):
+    """Returns the index of the smallest number in the list
+
+    Args:
+        input_list (list): List of numbers which are at least of length 1
+    Returns:
+        min_index (int): Index of list containing the smallest element of list
+    """
     if len(input_list) == 0:
         raise Exception("List must contain at least 1 element")
     
@@ -34,7 +50,16 @@ def smallest_elem_index(input_list):
     return min_index
 
 def selection_sort(input_list):
+    """Implementation of Selection Sort which orders a list of numbers
+
+    Args:
+        input_list (list): List of integers/floats
+    Returns:
+        input_list (list): Sorted list in an ascending order
+    """
     for i in range(len(input_list)-1):
+        ## By slicing the list incrementally, swap first element with lowest
+        ## element of sliced list
         temp_item = input_list[i]
         smallest_index = smallest_elem_index(input_list[i:])
         input_list[i] = input_list[smallest_index+i]
@@ -42,12 +67,24 @@ def selection_sort(input_list):
     return input_list
     
 def test_algorithm_runtime(algorithm, min_input, max_input, iterations):
+    """Implements an algorithm with data of various sizes to visualize
+    algorithm's running time
+
+    Args:
+        algorithm (method): Algorithm in form of function with first param that
+                            is list of data
+        min_input (int): Minimum length of data used in the algorithm's test
+        max_input (int): Maximum length of data used in the algorithm's test
+        iterations (int): Number of repetitions for each size of data
+    """
+
     test_sizes = [i for i in range(min_input, max_input+1)]
     test_times = []
     for i in range(min_input, max_input+1):
         test_list = [random.random() for j in range(i)]
 
-        ## Repeat algorithm with one size to improve accuracy
+        ## Repeat algorithm with one size "iterations" times to improve
+        ## accurracy
         time_sum = 0
         for j in range(iterations):
             start_time = datetime.now()
@@ -55,6 +92,7 @@ def test_algorithm_runtime(algorithm, min_input, max_input, iterations):
             time_sum+=((datetime.now() - start_time)*1e9).total_seconds()
         test_times.append(time_sum/iterations)
 
+    ## Plot running time results w/ matplotlib
     plot_test_time_scatter(test_sizes, test_times)
 
 def main():
