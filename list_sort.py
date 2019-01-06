8726a7e27423a6e9c9b22815cf2969b123046c29
AntonNiko <ant.nikitenko@gmail.com>
2019-01-06 14:00:36 -0800

    Implemented merge sort algorithm using two methods: merge_sort() and merge_sorted_lists()

--- list_sort.py
+++ list_sort.py
@@ -160,6 +160,55 @@ def shell_sort(input_list):
             input_list[::h] = h_sorted_list
             h = int(h/3)
     return input_list
+
+def merge_sorted_lists(list1, list2):
+    """Returns a list that is the merged version of 2 sorted lists
+    provided as arguments
+
+    Args:
+        list1 (list): Sorted list of numbers
+        list2 (list): Sorted list of numbers
+    Returns:
+        list1 (list): Sorted, merged version of the 2 lists
+    """
+    for j in range(len(list2)):
+        for i in range(len(list1)):
+            if list2[j] < list1[i]:
+                list1.insert(i,list2[j])
+                break
+        ## If element cannot be sorted anywhere inside the list, means
+        ## that it is greater than all elements. In that case, append to
+        ## list
+        if i==(len(list1)-1):
+            list1.append(list2[j])
+    return list1
+
+def merge_sort(input_list):
+    """Implements the merge sort algorithm to sort a list of numbers. Calls
+    itself in a recursive manner with smaller and smaller slices of the lists
+    until it can compare values of list of 2 elements
+    
+    Args:
+        input_list (list): A list of numbers that may or may not be ordered
+    Returns:
+        list_sorted (list): The sorted list of numbers
+    """
+    length = len(input_list)
+    if length == 2:
+        if input_list[0] > input_list[1]:
+            temp = input_list[1]
+            input_list[1] = input_list[0]
+            input_list[0] = temp
+        return input_list
+    elif length == 1:
+        return input_list
+    elif length == 0:
+        raise Exception("Length of list must be at least 1")
+    else:
+        left_sorted = merge_sort(input_list[0:int(length/2)])
+        right_sorted = merge_sort(input_list[int(length/2):])
+        list_sorted = merge_sorted_lists(left_sorted, right_sorted)
+        return list_sorted
     
 def test_algorithm_runtime(algorithm, min_input, max_input, iterations):
     """Implements an algorithm with data of various sizes to visualize
@@ -190,6 +239,7 @@ def test_algorithm_runtime(algorithm, min_input, max_input, iterations):
     plot_test_time_scatter_single(test_sizes, test_times)
     
 if __name__ == "__main__":
-    test_algorithm_runtime(shell_sort, 2, N, ITERATIONS)
-    ##a = [random.randrange(1,100) for i in range(20)]
-    ##print(shell_sort(a))
+    ##test_algorithm_runtime(shell_sort, 2, N, ITERATIONS)
+    a = [random.randrange(1,100) for i in range(20)]
+    print(merge_sort(a))
+    
